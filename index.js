5b29cfc8d15edf9f8d40805b53cd763d96bdd65c
xiomaraquinonez <40772612+xiomaraquinonez@users.noreply.github.com>
2020-03-06 14:55:56 -0800

    finished app

---
+++ index.js
@@ -0,0 +1,18 @@
+new Vue({
+  el: "#app",
+  data: {
+  	todos: [
+  		{ text: 'test', done: false, id: Date.now() }
+  	]
+  },
+  methods: {
+  	addTodo({target}){
+    	this.todos.push({text: target.value, done: false, id: Date.now()})
+      target.value = ''
+    },
+    removeTodo(id) {
+    	console.log('removeTodo has been clicked')
+    	this.todos = this.todos.filter(todo => todo.id !== id)
+    }
+  }
+})
