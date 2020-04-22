e75b42c609db0c2ac101afce480f166c94b5abc6
xiomaraquinonez <40772612+xiomaraquinonez@users.noreply.github.com>
2020-04-22 14:45:04 -0700

    button changes

--- index.js
+++ index.js
@@ -3,16 +3,31 @@ new Vue({
   data: {
   	todos: [
   		{ text: 'test', done: false, id: Date.now() }
-  	]
+  	], 
+    activeTodos: [], 
+    completedTodos: []
   },
   methods: {
   	addTodo({target}){
     	this.todos.push({text: target.value, done: false, id: Date.now()})
       target.value = ''
+      console.log(this.todos)
     },
     removeTodo(id) {
-    	console.log('removeTodo has been clicked')
     	this.todos = this.todos.filter(todo => todo.id !== id)
+    },
+    swapToCompleted() {
+      this.todos = this.activeTodos.concat(this.completedTodos) 
+      console.log(this.todos)
+      this.completedTodos = this.todos.filter(todo => todo.done == true)
+      this.activeTodos = this.todos.filter(todo => todo.done == false)
+      this.todos = this.completedTodos
+    },
+    swapToActive() {
+      this.todos = this.activeTodos.concat(this.completedTodos) 
+      this.completedTodos = this.todos.filter(todo => todo.done == true)
+      this.activeTodos = this.todos.filter(todo => todo.done == false)
+      this.todos = this.activeTodos
     }
   }
 }) 
